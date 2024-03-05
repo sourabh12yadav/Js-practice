90792cbf894e684d6af65fa8c50e7ead3e3ef528
sourabh12yadav <48904684+sourabh12yadav@users.noreply.github.com>
2024-03-05 11:58:07 +0000

    Comparrision

---
+++ js-basics/04-comparasion.js
@@ -0,0 +1,7 @@
+// console.log(null>0);// false null treated as a number here so 0>0 result false
+// console.log(null==0);// in case of equality null treated as undefined so it is also false
+// console.log(null>=0);//Again compariosn operator comes than it is treated as a number so 0>=0  its true
+
+console.log(undefined>0);//false  undefined is treated as a nan in case of any comparison
+console.log(undefined>=0); //false undefined is treated as a nan in case of any comparison
+console.log(undefined>=0);// false undefined is treated as a nan in case of any comparison
