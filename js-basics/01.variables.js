9bd5e39543ab21d7b3dda04e44ed6bf85e5a71ba
sourabh12yadav <48904684+sourabh12yadav@users.noreply.github.com>
2024-03-05 10:41:52 +0000

    datatypes

---
+++ js-basics/01.variables.js
@@ -0,0 +1,15 @@
+const accountId= 144553;
+let email="yadav@gamilcom"
+var accPassword=1234;
+accCity="Faridabad"
+
+email="me2@gmail.com"
+accPassword=9654
+accCity="Delhi"
+
+// accountId=2;--> Not Allowed you cannot assign anything to constant
+console.table([email, accPassword, accCity]);
+
+/*
+Preffer not use var because of issue in block scope and in functional scope
+*/
---
+++ js-basics/02-datatypes.js
@@ -0,0 +1,15 @@
+"use strict" //Treat all Js code with newer Version
+// alert("Hello"); -->We are using node js not browser.
+// Premitive data types
+//Number
+//bigint
+//String--> "" used in double quotes.
+//boolean => true/false
+//null => Stand alone value.
+//undefined ==> Value is not assigned yet.
+//Symbol==> to find unique 
+
+//Non premetive
+//Object
+console.log(typeof undefined);// output:- undefined
+console.log(typeof null);// output:- object
