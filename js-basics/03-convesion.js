f95b45ee98d54c8f4a55d1fb4e2ac38a2125e330
sourabh12yadav <48904684+sourabh12yadav@users.noreply.github.com>
2024-03-05 11:25:43 +0000

    operationas well

--- js-basics/03-convesion.js
+++ js-basics/03-convesion.js
@@ -3,11 +3,12 @@ let score= "Sourabh";
 // console.log(typeof score);
 // console.log(typeof (score));
 
-let value= Number(score);
+// let value= Number(score);
 
 // console.log(value);
 //"33" =>33
 //"33abc" => NAN
+
 //true =>1 , false=>0
 
 let isLoggedIn=""
@@ -16,12 +17,42 @@ let booleanIsLoggedIN= Boolean(isLoggedIn)
 
 // console.log(booleanIsLoggedIN);
 
+
 //1 => true , 0=> false
 //"" =>false
 //"sourabh" => true
 
-let someNumber=33;
+// let someNumber=33;
 
-let stringNumber= String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber= String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/******************************************Operation************************************************************ */
+// let value=3
+
+// let negValue= -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2*3);
+// console.log(2**3);
+
+let str1="Hello"
+
+let str2= " Sourabh"
+
+let str3=str1+str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// Special type conversion
+console.log(+true);
+console.log(+"");
+let num1 ,num2 ,num3
+
+num1 = num2 = num3 = 2+2;
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
