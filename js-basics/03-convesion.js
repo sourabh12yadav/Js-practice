cd082044a766f53cc081605bb683547276b2161c
sourabh12yadav <48904684+sourabh12yadav@users.noreply.github.com>
2024-03-05 11:01:17 +0000

    conversion

---
+++ js-basics/03-convesion.js
@@ -0,0 +1,27 @@
+let score= "Sourabh";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let value= Number(score);
+
+// console.log(value);
+//"33" =>33
+//"33abc" => NAN
+//true =>1 , false=>0
+
+let isLoggedIn=""
+
+let booleanIsLoggedIN= Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIN);
+
+//1 => true , 0=> false
+//"" =>false
+//"sourabh" => true
+
+let someNumber=33;
+
+let stringNumber= String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
